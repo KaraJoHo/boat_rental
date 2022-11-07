9e35a3890a4fe321c329e64054f207b395496096
KaraJH <karas.jones@yahoo.com>
2022-11-06 18:33:39 -0700

    Adds tests for dock, boat and renter

--- spec/boat_spec.rb
+++ spec/boat_spec.rb
@@ -9,4 +9,26 @@ RSpec.describe Boat do
     expect(kayak.type).to eq(:kayak)
     expect(kayak.price_per_hour).to eq(20)
   end
+
+  describe '#hours_rented' do
+    it 'shows the number of hours rented' do
+      kayak = Boat.new(:kayak, 20)
+
+      expect(kayak.hours_rented).to eq(0)
+    end
+  end
+
+  describe '#add_hour' do
+    it 'adds an hour to the hours rented' do
+      kayak = Boat.new(:kayak, 20)
+
+      expect(kayak.hours_rented).to eq(0)
+
+      kayak.add_hour
+      kayak.add_hour
+      kayak.add_hour
+
+      expect(kayak.hours_rented).to eq(3)
+    end
+  end
 end
